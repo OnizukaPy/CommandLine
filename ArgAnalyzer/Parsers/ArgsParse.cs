774c038c285afa3d835e3bb1d5474c209073f6fc
OnizukaPy <perito@ivancatalano.eu>
2025-03-28 17:08:23 +0100

    Aggiunto al Paser il metodo per trovare se ci sono errori o comandi errati e il metodo per ottenere il valori delle opzioni

--- ArgAnalyzer/Parsers/ArgsParse.cs
+++ ArgAnalyzer/Parsers/ArgsParse.cs
@@ -25,5 +25,47 @@ public class ArgsParser {
         _subCommands = _rootCommand.Commands != null ? _rootCommand.Commands : null;
 
     }
+
+        // metodo per ottenere il valore da un comando passato
+    public string GetOptionValue(string[] optionAliases, List<string> argsList) {
+        //List<string> argList = _args != null ? _args.ToList() : new ();
+        string value = String.Empty;
+        for (int i = 0; i < argsList.Count(); i++) {
+            if (optionAliases.Contains(argsList[i])) {
+                if (i + 1 < argsList.Count())
+                    if (!argsList[i+1].StartsWith("-"))
+                        value =  argsList[i+1];
+            }
+        }
+        return value;
+    }
+
+    // metodo per verificare se ci sono comandi inattesi
+    public bool FindStrangers(List<string> argsList, out string value) {
+
+        if( _rootCommand !=  null && 
+            _rootCommand.options != null) {
+            
+            List<Option> rootOption = _rootCommand.options;
+            List<string> rootOptionsAliases = new();
+            rootOption.ForEach(ro => rootOptionsAliases.AddRange(ro.Aliases.ToList()));
+
+            // cicliamo la lista
+            for (int i = 0; i < argsList.Count(); i++) {
+                
+                string arg = argsList[i];
+                bool check = false;
+
+                if (arg.StartsWith("-") && !rootOptionsAliases.Contains(arg)) {
+                    check = true;
+                    value = arg;
+                    Console.WriteLine($"Argomento in esame: {arg}, {check}");
+                    return true;
+                }
+            }
+        }
+        value = String.Empty;
+        return false;
+    }
 }
 
